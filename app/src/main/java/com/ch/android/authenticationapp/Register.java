c724abe4922d6c2ec2dfdf7f915bd68c01b5e6b5
Ciprian Hotea <cipry13>
2020-07-04 16:42:57 +0100

    Added Firebase Firestore Save user's data in Firestore Database

--- app/src/main/java/com/ch/android/authenticationapp/Register.java
+++ app/src/main/java/com/ch/android/authenticationapp/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,15 +14,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
-    EditText mFullName, mEmail, mPassword;
+    EditText mFullName, mEmail, mPassword, mPhoneNumber;
     Button mRegisterButton;
     TextView mLoginButton;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
 
     @Override
@@ -34,8 +44,10 @@ public class Register extends AppCompatActivity {
         mPassword = findViewById(R.id.edittext_password);
         mRegisterButton = findViewById(R.id.register_button);
         mLoginButton = findViewById(R.id.login_text_message);
+        mPhoneNumber = findViewById(R.id.edittext_phone_number);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -45,8 +57,10 @@ public class Register extends AppCompatActivity {
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                final String fullName = mFullName.getText().toString();
+                final String phoneNumber = mPhoneNumber.getText().toString();
 
                 if(TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is required.");
@@ -66,6 +80,23 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "User created.", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("name", fullName);
+                            user.put("email", email);
+                            user.put("phone", phoneNumber);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("DB", "onSuccess: user profile is created for :" + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("DB", "onFailure: " + e.toString());
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         } else {
                             Toast.makeText(Register.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
