22eaa613fcf40a9b69b31f7b1c5aa4c03616419a
Ciprian Hotea <cipry13>
2020-07-07 20:02:00 +0100

    Change password implemented

--- app/src/main/java/com/ch/android/authenticationapp/MainActivity.java
+++ app/src/main/java/com/ch/android/authenticationapp/MainActivity.java
@@ -2,13 +2,16 @@ package com.ch.android.authenticationapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
-    Button verifyEmailButton;
+    Button verifyEmailButton, changePasswordButton;
+    FirebaseUser fUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         name = findViewById(R.id.profile_name);
         email = findViewById(R.id.profile_email);
         phone = findViewById(R.id.profile_phone);
+        changePasswordButton = findViewById(R.id.change_password);
 
 
         verifyMessage = findViewById(R.id.not_verified);
@@ -46,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         fStore = FirebaseFirestore.getInstance();
 
         userId = fAuth.getCurrentUser().getUid();
-        final FirebaseUser fUser = fAuth.getCurrentUser();
+        fUser = fAuth.getCurrentUser();
 
         if(!fUser.isEmailVerified()){
             verifyMessage.setVisibility(View.VISIBLE);
@@ -74,13 +79,51 @@ public class MainActivity extends AppCompatActivity {
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                if(e!=null) {
+                if(e != null) {
                     Log.d("MainActivity", "Error: " + e.getMessage());
                 } else {
                     name.setText(documentSnapshot.getString("name"));
                     email.setText(documentSnapshot.getString("email"));
                     phone.setText(documentSnapshot.getString("phone"));
                 }
+            }
+        });
+
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText changePassword = new EditText(v.getContext());
+                AlertDialog.Builder passwordChangeDialog = new AlertDialog.Builder(v.getContext());
+                passwordChangeDialog.setTitle("Change password ?");
+                passwordChangeDialog.setMessage("Enter new password");
+                passwordChangeDialog.setView(changePassword);
+
+                passwordChangeDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newPassword = changePassword.getText().toString();
+                        fUser.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this,"Password changed successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this,"Password change failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                passwordChangeDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                passwordChangeDialog.create().show();
             }
         });
     }
