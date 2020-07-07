9a645f78af3c1ddab918a7a5da81bb7cd4ff9ce1
Ciprian Hotea <cipry13>
2020-07-07 13:18:32 +0100

    Added User Profile Retrieve data from Firebase Firestore

--- app/src/main/java/com/ch/android/authenticationapp/MainActivity.java
+++ app/src/main/java/com/ch/android/authenticationapp/MainActivity.java
@@ -1,19 +1,48 @@
 package com.ch.android.authenticationapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+    TextView name, email, phone;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.profile_name);
+        email = findViewById(R.id.profile_email);
+        phone = findViewById(R.id.profile_phone);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                name.setText(documentSnapshot.getString("name"));
+                email.setText(documentSnapshot.getString("email"));
+                phone.setText(documentSnapshot.getString("phone"));
+            }
+        });
+
     }
 
     public void logout(View view){
